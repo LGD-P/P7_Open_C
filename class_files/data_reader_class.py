53de85bcc603eaaf064580dfed8afbf1823e244d
LGD-P <nplgdjp@gmx.com>
2023-04-16 16:50:26 +0200

    clear files folders

---
+++ class_files/data_reader_class.py
@@ -0,0 +1,70 @@
+import pandas as pd
+from rich.console import Console
+
+c = Console()
+
+
+class Reader:
+    @staticmethod
+    def csv_reader(csv_file_name):
+        """This method is just a .csv reader
+        with pandas library
+
+        Args:
+            csv_file_name (str): "action.csv"
+
+        Returns:
+            class: pandas object readable
+        """
+        data_frame = pd.read_csv(csv_file_name)
+        return data_frame
+
+    @staticmethod
+    def creat_list_from_culumn(df, culumn):
+        """This method creat a list of data
+        from .csv column choosed by user
+
+        Args:
+            culumn (str): header chooses "Action #"
+
+        Returns:
+            list: list of culumn content
+        """
+        list_from_culumn = []
+        for element in df[culumn]:
+            try:
+                list_from_culumn.append(int(element.strip("%")))
+            except (AttributeError, ValueError):
+                list_from_culumn.append(element)
+        return list_from_culumn
+
+    @staticmethod
+    def return_price_action_percentage(csv_file, action_culumn, price_culumn, percentage_culumn):
+        """This methode return all array culumns in variable list
+
+        Args:
+            csv_file (path): path to csv file
+            action_culumn (header): for column action name
+            price_culumn (header): for column price
+            percentage_culumn (header): for %/2years
+
+        Returns:
+            list: one list for each column
+        """
+        df = Reader.csv_reader(csv_file)
+        dfAction = Reader.creat_list_from_culumn(df, action_culumn)
+        dfPrice = Reader.creat_list_from_culumn(df, price_culumn)
+        dfPercentage = Reader.creat_list_from_culumn(df, percentage_culumn)
+        return dfAction, dfPrice, dfPercentage
+
+    @staticmethod
+    def display_result(execution_duration, best_price, best_profit, action_name_list):
+        c.print("[bold green3] - La meilleur combinaison d'Actions pour un investissement maximum de 500€ est:\
+                \n[bold green3]")
+        c.print(f"[bold yellow] - {action_name_list}\n[bold yellow]")
+        c.print(
+            f"[bold green3] - Pour un coût de: [bold yellow]{best_price}[bold yellow] €\n")
+        c.print(
+            f"[bold green3] - Et une retabilité de: [bold yellow]{round(best_profit,2)}[bold yellow] € / 2ans\n")
+        c.print(
+            f"[bold green3] - Le temps d'exécution de l'algoritme est de:[bold yellow] {execution_duration} sec.")
---
+++ class_files/optimized_class.py
@@ -0,0 +1,60 @@
+import time
+
+
+class Optimized:
+    @staticmethod
+    def creat_dict_with_yield(defPrice, defPercentage, defAction):
+        """This function creat in a list, for each action, a dictionary
+        with action name, price, percentage, and yield
+
+        Args:
+            defPrice (list): price list
+            defPercentage (list): percentage list
+            defAction (list): action name list
+
+        Returns:
+            _type_: _description_
+        """
+        result = []
+        actionYield = []
+        index = -1
+        for element in range(len(defAction)):
+            index += 1
+
+            actionYield.append(
+                round((defPrice[element] * (defPercentage[element] / 100)), 2))
+
+            result.append({'Action': defAction[index],
+                           'Price': defPrice[index],
+                           'Percentage': defPercentage[index],
+                           'Yield': actionYield[index]})
+
+        sorted_result = sorted(result, key=lambda x: -x['Yield'])
+
+        return sorted_result
+
+    @staticmethod
+    def algo_glouton(max_investment, list_of_data):
+        """This function implemant glouton algoritm to a
+        a list of dictionary.
+
+        Args:
+            max_investment (int): max user investment
+            list_of_data (dict list): list of dict for each data action name price percentage and yield
+
+        Returns:
+            Best Invest : spent as int, action_to_buy as list, benefit as flaot, execution time
+        """
+        spent = 0
+        action_to_buy = []
+        benefit = 0
+        start_time = time.time()
+        for element in range(len(list_of_data)):
+            if list_of_data[element]["Price"] > 0.0 and spent + list_of_data[element]["Price"] <= max_investment:
+                spent += list_of_data[element]["Price"]
+                benefit += list_of_data[element]["Yield"]
+                action_to_buy.append(list_of_data[element]["Action"])
+        execution_time = round(time.time() - start_time)
+        if execution_time < 1:
+            execution_time = "Moins d'une Seconde"
+        return spent, action_to_buy, round(benefit, 2), execution_time
